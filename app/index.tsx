fce705945d0a91afa0b679babca21ac586834ce2
javeria2108 <javeriazh@gmail.com>
2024-05-31 21:21:53 +0500

    react navigation added

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,10 @@
 import { Text, View } from "react-native";
 
 export default function Index() {
+  
   return (
-    <View
+    
+       <View
       style={{
         flex: 1,
         justifyContent: "center",
