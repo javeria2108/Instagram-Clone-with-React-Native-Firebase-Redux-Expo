bca874fd0841c576d9bd2fbca79ec70ec3432e82
javeria2108 <javeriazh@gmail.com>
2024-06-01 22:18:18 +0500

    register page

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,17 @@
-import { Text, View } from "react-native";
+import { Text, View,Button } from "react-native";
 
 export default function Index() {
   
   return (
+    <View style={{ flex: 1, justifyContent: "center" }}>
+    <Button
+      title="Register"
+      
+    />
+    <Button
+      title="Login"
     
-       <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    />
+  </View>
   );
 }
